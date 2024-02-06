7492573ec6c3f68c1485807e57969bab7906423a
Angel Fco <angel02fco@gmail.com>
2024-02-05 18:37:31 -0600

    añadir solicitud de verificación de correo electrónico

---
+++ src/domains/email_verification/controller.js
@@ -0,0 +1,25 @@
+const User = require("./../user/model");
+const { sendOTP } = require("./../otp/controller");
+
+const sendVerificationOTPEmail = async (email) => {
+    try {
+        // check if an account exits
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw Error("Theres no account for the provided email.");
+        }
+
+        const otpDetails = {
+            email,
+            subject: "Email Verification",
+            message: "Verify your email with the code below",
+            duration: 1
+        };
+        const createdOTP = await sendOTP(otpDetails);
+        return createdOTP;
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = { sendVerificationOTPEmail };
