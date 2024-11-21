efca92d7b302d8bf2890e4960b5e44f6a3d11c9e
Angel Fco <angel02fco@gmail.com>
2024-11-21 16:39:37 -0600

    actualizacion del backend

--- src/domains/citas/model.js
+++ src/domains/citas/model.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CitaSchema = new Schema({
-    usuario: { type: Schema.Types.ObjectId, ref: 'User' },
-    mascota: { type: Schema.Types.ObjectId, ref: 'Pet' },
-    servicio: { type: Schema.Types.ObjectId, ref: 'Service' },
-    medico: { type: Schema.Types.ObjectId, ref: 'Veterinario' },
-    fecha: { type: String, require: true },
-    hora: { type: String, require: true },
-    comentarios: { type: String },
-    state: { type: Boolean, default: true },
-    citaCreated : { type: String },
-    estado: { type: String, default: "proxima" },
-    opinionUsuario: { type: Boolean, default: false }
+  usuario: { type: Schema.Types.ObjectId, ref: "User" },
+  mascota: { type: Schema.Types.ObjectId, ref: "Pet" },
+  servicio: { type: Schema.Types.ObjectId, ref: "Service" },
+  medico: { type: Schema.Types.ObjectId, ref: "Veterinario" },
+  fecha: { type: String, require: true },
+  hora: { type: String, require: true },
+  comentarios: { type: String },
+  state: { type: Boolean, default: true },
+  citaCreated: { type: String },
+  estado: { type: String, default: "proxima" },
+  opinionUsuario: { type: Boolean, default: true },
 });
 
 const Cita = mongoose.model("Cita", CitaSchema);
