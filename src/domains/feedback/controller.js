4afbe031fed00a7e342d581cbfab2cd22e5f2e2b
Angel Fco <angel02fco@gmail.com>
2024-11-22 10:34:05 -0600

    actualizacion del backend

--- src/domains/feedback/controller.js
+++ src/domains/feedback/controller.js
@@ -21,4 +21,281 @@ const sendFeedbackPostAgendamiento = async (data) => {
   }
 };
 
-module.exports = { sendFeedbackPostAgendamiento };
+const getPregunta1StatsWeb = async () => {
+  try {
+    const stats = await Feedback.aggregate([
+      {
+        $match: { dispositivo: "web" },
+      },
+      {
+        $group: {
+          _id: "$pregunta1",
+          total: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          title: "$_id",
+          votes: "$total",
+          color: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$_id", 1] }, then: "bg-indigo-500" },
+                { case: { $eq: ["$_id", 2] }, then: "bg-fuchsia-500" },
+                { case: { $eq: ["$_id", 3] }, then: "bg-violet-500" },
+              ],
+              default: "bg-gray-500",
+            },
+          },
+        },
+      },
+    ]);
+
+    const formattedStats = [1, 2, 3].map((title) => {
+      const stat = stats.find((s) => s.title === title);
+
+      // Mapeo de títulos numéricos a textos
+      let label = "";
+      if (title === 1) label = "Fácil";
+      if (title === 2) label = "Regular";
+      if (title === 3) label = "Difícil";
+
+      return {
+        title: label, // Asignamos el título textual
+        votes: stat ? stat.votes : 0,
+        color: stat ? stat.color : "bg-gray-500",
+      };
+    });
+
+    return formattedStats;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getPregunta2StatsWeb = async () => {
+  try {
+    const stats = await Feedback.aggregate([
+      { $match: { dispositivo: "web" } },
+      { $group: { _id: "$pregunta2", total: { $sum: 1 } } },
+      {
+        $project: {
+          _id: 0,
+          title: "$_id",
+          votes: "$total",
+          color: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$_id", 1] }, then: "bg-indigo-500" },
+                { case: { $eq: ["$_id", 2] }, then: "bg-fuchsia-500" },
+              ],
+              default: "bg-gray-500",
+            },
+          },
+        },
+      },
+    ]);
+
+    const formattedStats = [1, 2].map((title) => {
+      const stat = stats.find((s) => s.title === title);
+      let label = "";
+      if (title === 1) label = "Sí";
+      if (title === 2) label = "No";
+
+      return {
+        title: label,
+        votes: stat ? stat.votes : 0,
+        color: stat ? stat.color : "bg-gray-500",
+      };
+    });
+
+    return formattedStats;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getPregunta3StatsWeb = async () => {
+  try {
+    const stats = await Feedback.aggregate([
+      { $match: { dispositivo: "web" } },
+      { $group: { _id: "$pregunta3", total: { $sum: 1 } } },
+      {
+        $project: {
+          _id: 0,
+          title: "$_id",
+          votes: "$total",
+          color: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$_id", 1] }, then: "bg-indigo-500" },
+                { case: { $eq: ["$_id", 2] }, then: "bg-fuchsia-500" },
+                { case: { $eq: ["$_id", 3] }, then: "bg-violet-500" },
+              ],
+              default: "bg-gray-500",
+            },
+          },
+        },
+      },
+    ]);
+
+    const formattedStats = [1, 2, 3].map((title) => {
+      const stat = stats.find((s) => s.title === title);
+      let label = "";
+      if (title === 1) label = "Buena";
+      if (title === 2) label = "Neutral";
+      if (title === 3) label = "Mala";
+
+      return {
+        title: label,
+        votes: stat ? stat.votes : 0,
+        color: stat ? stat.color : "bg-gray-500",
+      };
+    });
+
+    return formattedStats;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getPregunta1StatsMovil = async () => {
+  try {
+    const stats = await Feedback.aggregate([
+      { $match: { dispositivo: "movil" } },
+      { $group: { _id: "$pregunta1", total: { $sum: 1 } } },
+      {
+        $project: {
+          _id: 0,
+          title: "$_id",
+          votes: "$total",
+          color: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$_id", 1] }, then: "bg-indigo-500" },
+                { case: { $eq: ["$_id", 2] }, then: "bg-fuchsia-500" },
+                { case: { $eq: ["$_id", 3] }, then: "bg-violet-500" },
+              ],
+              default: "bg-gray-500",
+            },
+          },
+        },
+      },
+    ]);
+
+    const formattedStats = [1, 2, 3].map((title) => {
+      const stat = stats.find((s) => s.title === title);
+      let label = "";
+      if (title === 1) label = "Fácil";
+      if (title === 2) label = "Regular";
+      if (title === 3) label = "Difícil";
+
+      return {
+        title: label, // Asignamos el título textual
+        votes: stat ? stat.votes : 0,
+        color: stat ? stat.color : "bg-gray-500",
+      };
+    });
+
+    return formattedStats;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getPregunta2StatsMovil = async () => {
+  try {
+    const stats = await Feedback.aggregate([
+      { $match: { dispositivo: "movil" } },
+      { $group: { _id: "$pregunta2", total: { $sum: 1 } } },
+      {
+        $project: {
+          _id: 0,
+          title: "$_id",
+          votes: "$total",
+          color: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$_id", 1] }, then: "bg-indigo-500" },
+                { case: { $eq: ["$_id", 2] }, then: "bg-fuchsia-500" },
+              ],
+              default: "bg-gray-500",
+            },
+          },
+        },
+      },
+    ]);
+
+    const formattedStats = [1, 2].map((title) => {
+      const stat = stats.find((s) => s.title === title);
+      let label = "";
+      if (title === 1) label = "Sí";
+      if (title === 2) label = "No";
+
+      return {
+        title: label,
+        votes: stat ? stat.votes : 0,
+        color: stat ? stat.color : "bg-gray-500",
+      };
+    });
+
+    return formattedStats;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getPregunta3StatsMovil = async () => {
+  try {
+    const stats = await Feedback.aggregate([
+      { $match: { dispositivo: "movil" } },
+      { $group: { _id: "$pregunta3", total: { $sum: 1 } } },
+      {
+        $project: {
+          _id: 0,
+          title: "$_id",
+          votes: "$total",
+          color: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$_id", 1] }, then: "bg-indigo-500" },
+                { case: { $eq: ["$_id", 2] }, then: "bg-fuchsia-500" },
+                { case: { $eq: ["$_id", 3] }, then: "bg-violet-500" },
+              ],
+              default: "bg-gray-500",
+            },
+          },
+        },
+      },
+    ]);
+
+    const formattedStats = [1, 2, 3].map((title) => {
+      const stat = stats.find((s) => s.title === title);
+      let label = "";
+      if (title === 1) label = "Buena";
+      if (title === 2) label = "Neutral";
+      if (title === 3) label = "Mala";
+
+      return {
+        title: label,
+        votes: stat ? stat.votes : 0,
+        color: stat ? stat.color : "bg-gray-500",
+      };
+    });
+
+    return formattedStats;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = {
+  sendFeedbackPostAgendamiento,
+  getPregunta1StatsWeb,
+  getPregunta2StatsWeb,
+  getPregunta3StatsWeb,
+  getPregunta1StatsMovil,
+  getPregunta2StatsMovil,
+  getPregunta3StatsMovil,
+};
--- src/domains/feedback/routes.js
+++ src/domains/feedback/routes.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { sendFeedbackPostAgendamiento } = require("./controller");
+const {
+  sendFeedbackPostAgendamiento,
+  getPregunta1StatsWeb,
+  getPregunta2StatsWeb,
+  getPregunta3StatsWeb,
+  getPregunta1StatsMovil,
+  getPregunta2StatsMovil,
+  getPregunta3StatsMovil,
+} = require("./controller");
 
 router.post("/", async (req, res) => {
   try {
@@ -21,4 +29,57 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/web/pregunta1", async (req, res) => {
+  try {
+    const feedback = await getPregunta1StatsWeb();
+    res.status(200).json(feedback);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.get("/web/pregunta2", async (req, res) => {
+  try {
+    const feedback = await getPregunta2StatsWeb();
+    res.status(200).json(feedback);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.get("/web/pregunta3", async (req, res) => {
+  try {
+    const feedback = await getPregunta3StatsWeb();
+    res.status(200).json(feedback);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.get("/movil/pregunta1", async (req, res) => {
+  try {
+    const feedback = await getPregunta1StatsMovil();
+    res.status(200).json(feedback);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.get("/movil/pregunta2", async (req, res) => {
+  try {
+    const feedback = await getPregunta2StatsMovil();
+    res.status(200).json(feedback);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.get("/movil/pregunta3", async (req, res) => {
+  try {
+    const feedback = await getPregunta3StatsMovil();
+    res.status(200).json(feedback);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 module.exports = router;
